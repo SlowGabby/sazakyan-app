3b21b9fd94c65903d04d8efae6492ac486379508
Ranier Arcega <rdavee0412@gmail.com>
2023-09-22 11:32:29 +0800

    updated

--- app/src/main/java/com/example/sazakyanapp/DBHelper.kt
+++ app/src/main/java/com/example/sazakyanapp/DBHelper.kt
@@ -1,5 +1,6 @@
 package com.example.sazakyanapp
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -9,7 +10,42 @@ class DBHelper(context: Context):SQLiteOpenHelper(context, "Userdata", null, 1) 
         p0?.execSQL("create table Userdata (username TEXT primary key, password TEXT)")
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        p0?.execSQL("drop table if exists Userdata")
+    }
+
+    fun insertData (username: String, password: String): Boolean {
+
+        val p0 = this.writableDatabase
+        val cv = ContentValues()
+        cv.put("username", username)
+        cv.put("password", password)
+        val result = p0.insert("Userdata", null, cv)
+
+        if (result == -1 .toLong()) {
+            return false
+        }
+
+        return true
 
     }
+
+    fun checkUserPassword (username: String, password: String) : Boolean {
+
+        val p0 = this.writableDatabase
+        val query = "select * from Userdata where username= '$username' and password= '$password'"
+        val cursor = p0.rawQuery(query, null)
+
+        if (cursor.count <= 0) {
+            cursor.close()
+            return false
+        }
+
+        cursor.close()
+        return true
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/sazakyanapp/SignupFragment.kt
+++ app/src/main/java/com/example/sazakyanapp/SignupFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 import com.example.sazakyanapp.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
 
     private lateinit var Binding : FragmentSignupBinding
+    private lateinit var userName : EditText
+    private lateinit var passWord : EditText
+    private lateinit var checkPassword : EditText
+    private lateinit var db : DBHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
 
         Binding = FragmentSignupBinding.inflate(inflater, container, false)
 
