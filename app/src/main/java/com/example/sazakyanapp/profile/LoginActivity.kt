4ccc7efd93ffbc69f6a5ac2c3b4cb0ee059b8760
Ranier Arcega <rdavee0412@gmail.com>
2023-10-02 12:35:01 +0800

    implemented database

--- app/src/main/java/com/example/sazakyanapp/profile/LoginActivity.kt
+++ app/src/main/java/com/example/sazakyanapp/profile/LoginActivity.kt
@@ -1,12 +1,55 @@
 package com.example.sazakyanapp.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.sazakyanapp.CarsActivity
+import com.example.sazakyanapp.HomeActivity
+import com.example.sazakyanapp.MainHomeActivity
 import com.example.sazakyanapp.R
+import com.example.sazakyanapp.database.DBHelper
+import com.example.sazakyanapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var dbHelper : DBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_login)
+
+        dbHelper = DBHelper(this)
+
+        binding.loginBtn.setOnClickListener {
+
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = binding.loginPassword.text.toString()
+            loginDatabase(loginUsername, loginPassword)
+
+        }
+
+        binding.signupRedirect.setOnClickListener {
+
+            startActivity(Intent(this@LoginActivity, SignUpActivity::class.java))
+            finish()
+
+        }
+    }
+
+    private fun loginDatabase (username : String, password : String) {
+
+        val userExists = dbHelper.readUser(username, password)
+        if (userExists) {
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this@LoginActivity, CarsActivity::class.java))
+
+        } else {
+            Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/sazakyanapp/profile/SignUpActivity.kt
+++ app/src/main/java/com/example/sazakyanapp/profile/SignUpActivity.kt
@@ -1,12 +1,55 @@
 package com.example.sazakyanapp.profile
 
+import android.content.Intent
+import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.sazakyanapp.R
+import com.example.sazakyanapp.database.DBHelper
+import com.example.sazakyanapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySignUpBinding
+    private lateinit var dbHelper: DBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_sign_up)
+
+        dbHelper = DBHelper(this)
+
+        binding.signupBtn.setOnClickListener {
+
+            val signupUsername = binding.signupUsername.text.toString()
+            val signupPassword = binding.signupPassword.text.toString()
+            signupDatabase(signupUsername, signupPassword)
+
+        }
+
+        binding.signupBtn.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+    }
+
+    private fun signupDatabase(username : String, password : String) {
+
+        val insertedRowId = dbHelper.insertUser(username, password)
+        if (insertedRowId != -1L) {
+            Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@SignUpActivity, LoginActivity::class.java))
+            finish()
+
+        } else {
+            Toast.makeText(this, "Signup Failed!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
