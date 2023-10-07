4985d7a13a214355835b14575d807afb06de058c
Ranier Arcega <rdavee0412@gmail.com>
2023-10-07 16:25:19 +0800

    final layout homepage

--- app/src/main/java/com/example/sazakyanapp/MainActivity.kt
+++ app/src/main/java/com/example/sazakyanapp/MainActivity.kt
@@ -3,26 +3,32 @@ package com.example.sazakyanapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-
-    lateinit var bottomNav : BottomNavigationView
+    private lateinit var imageSlider : ImageSlider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Thread.sleep(3000)
+        Thread.sleep(1500)
         installSplashScreen()
 
         setContentView(R.layout.activity_main)
 
-        bottomNav = findViewById(R.id.bottomNav)
+        imageSlider = findViewById(R.id.imageSlider)
 
-        bottomNav.setOnClickListener {
+        val imagesList = ArrayList<SlideModel>()
+        imagesList.add(SlideModel("https://cdn.sixt.io/cms/originals/cebff417-98e6-4f28-b698-feeea6b46fd5.png"))
+        imagesList.add(SlideModel("https://socar-my-website.s3.ap-southeast-1.amazonaws.com/socar-my/Rjra4EF3kEqKHeOBAoDJqg2PFmEiqacN5PS53pct.jpeg"))
+        imagesList.add(SlideModel("https://rayyancarrental.com/wp-content/uploads/2023/03/promo-raya-.png"))
 
-        }
+        imageSlider.setImageList(imagesList, ScaleTypes.CENTER_INSIDE)
+
 
     }
 }
